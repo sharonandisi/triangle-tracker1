33fce61a26c2fe5cf3f9c2d1dc203b670a80066f
sharonandisi <sharonandisi.28@gmail.com>
2018-09-14 16:50:33 +0300

    edi background images

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ function tracker() {
     var sideTwo = parseInt(document.getElementById("side2").value);
     var sideThree = parseInt(document.getElementById("side3").value);
     var ans=document.getElementById("answer");
+    var image = document.getElementsByTagName("img");
     if((sideOne + sideTwo)>sideThree && (sideOne + sideThree)>sideTwo && (sideTwo + sideThree)>sideOne){
         if(sideOne === sideTwo && sideTwo === sideThree){
-            ans.textContent = "Equilateral Triangle"
+            ans.textContent = "Equilateral Triangle" 
+            
         }
         else if(sideOne === sideTwo || sideOne === sideThree || sideTwo === sideThree){
-            ans.textContent = "Isoscceles"
+            ans.textContent = "Isosceles"
         }
         else if(sideOne !== sideTwo && sideTwo !== sideThree && sideThree !== sideOne) {
-            ans.textContent= "Scalene"
+            ans.textContent= "Scalene" 
+            
 
         }
     
