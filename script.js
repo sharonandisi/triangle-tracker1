6202d332e4f7d825f8c90bd61263b3c8872149e3
sharonandisi <sharonandisi.28@gmail.com>
2018-09-14 13:12:57 +0300

    add first if statement

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ function tracker() {
     var sideOne = parseInt(document.getElementById("side1").value);
     var sideTwo = parseInt(document.getElementById("side2").value);
     var sideThree = parseInt(document.getElementById("side3").value);
-    if(sideOne)
+    if((sideOne + sideTwo)>sideThree && (sideone + sideThree)>sideTwo && (sideTwo + sideThree)>sideOne){
+        if(sideOne === sideTwo && sideTwo === sideThree){
+            ans.textContent = "Equilateral Triangle"
+        }
+        else if()
+    }
 }
